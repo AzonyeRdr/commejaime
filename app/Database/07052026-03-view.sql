e20308a1e67d0232eda8cd581693bb88dacfc970
randrianasolo-faniry-niaina <randrianasolofaniry6@gmail.com>
2026-05-08 08:48:37 +0300

    creation view et diagramme BdD

---
+++ app/Database/07052026-03-view.sql
@@ -0,0 +1,9 @@
+-- Active: 1741104595338@@127.0.0.1@3306@commejaime
+create view v_ingredientRegime as (
+    select i.lib as ingredient, r.nomplat as regime, ir.pourcentage as pourcentage, i.prixG as prixG, r.`poidsTotalPlat` as poidsTotalPlat
+    from ingredients i
+    join ingredientRegime ir on i.id = ir.ingredientId
+    join regime r on r.id = ir.regimeId
+);
+
+
