154aae6f7bbcb4274a5de59f496fe1d1d73ed759
randrianasolo-faniry-niaina <randrianasolofaniry6@gmail.com>
2026-05-08 11:10:08 +0300

    correction view

--- app/Database/07052026-03-view.sql
+++ app/Database/07052026-03-view.sql
@@ -1,6 +1,6 @@
 -- Active: 1741104595338@@127.0.0.1@3306@commejaime
-create view v_ingredientRegime as (
-    select i.lib as ingredient, r.nomplat as regime, ir.pourcentage as pourcentage, i.prixG as prixG, r.`poidsTotalPlat` as poidsTotalPlat
+create or replace view v_ingredientRegime as (
+    select r.id as `regimeId`,i.lib as ingredient, r.nomplat as regime, ir.pourcentage as pourcentage, i.prixG as prixG, r.`poidsTotalPlat` as poidsTotalPlat
     from ingredients i
     join ingredientRegime ir on i.id = ir.ingredientId
     join regime r on r.id = ir.regimeId
