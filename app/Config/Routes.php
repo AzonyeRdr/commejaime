4b84038299e0bea1d2ae415d7e041e41f76861a9
AzonyeRdr <randrianarivelonatha@gmail.com>
2026-05-08 19:38:19 +0300

    filtre finish

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,15 +5,20 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+$routes->get('/', 'AuthController::inscriptionForm');
+$routes->post('/', 'AuthController::inscrire');
+$routes->get('/login', 'AuthController::loginForm');
+$routes->post('/login', 'AuthController::login');
+
+
+$routes->group('', ['filter' => 'auth'], function ($routes) {
+    $routes->get('/index', 'Home::index');
+});
+
 $routes->group('admin', ['filter' => 'role:1'], function ($routes) {
     $routes->get(
         '/',
         'AdminController::index'
     );
 });
-
-$routes->get('/', 'AuthController::inscriptionForm');
-$routes->post('/', 'AuthController::inscrire');
-$routes->get('/login', 'AuthController::loginForm');
-$routes->post('/login', 'AuthController::login');
-$routes->get('/index', 'Home::index');
--- app/Views/home.php
+++ app/Views/home.php
@@ -5,7 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 </head>
+<?php $success = session()->getFlashdata('success') ?? '' ?>
 <body>
-    <p>Bonjour</p>
+    <div class="success">
+        <p><?= $success ?></p>
+    </div>
+    Bonjour
 </body>
 </html>
