27ec927485eceddd6ed24fb23daeb2940dd19c08
AzonyeRdr <randrianarivelonatha@gmail.com>
2026-05-08 19:53:20 +0300

    modified:   app/Config/Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,14 +11,20 @@ $routes->post('/', 'AuthController::inscrire');
 $routes->get('/login', 'AuthController::loginForm');
 $routes->post('/login', 'AuthController::login');
 
-
+//Route pour ceux qui sont connectés
 $routes->group('', ['filter' => 'auth'], function ($routes) {
     $routes->get('/index', 'Home::index');
 });
 
+//Routes pour admin, (role=1)
 $routes->group('admin', ['filter' => 'role:1'], function ($routes) {
     $routes->get(
         '/',
         'AdminController::index'
     );
+});
+
+//Routes pour gold, (role=2)
+$routes->group('', ['filter' => 'role:2'], function ($routes) {
+
 });
