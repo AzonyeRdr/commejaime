c842df581d39537dcd29691821c126d1ef9f2d00
randrianasolo-faniry-niaina <randrianasolofaniry6@gmail.com>
2026-05-11 18:49:58 +0300

    sql

--- app/Database/07052026-01-init.sql
+++ app/Database/07052026-01-init.sql
@@ -1,3 +1,3 @@
--- Active: 1770031521814@@127.0.0.1@3306@commejaime
+
 CREATE DATABASE commejaime;
 use commejaime;
--- app/Database/07052026-04-view.sql
+++ app/Database/07052026-04-view.sql
@@ -1,4 +1,4 @@
--- Active: 1770031521814@@127.0.0.1@3306@commejaime
+-- Active: 1741104595338@@127.0.0.1@3306@commejaime
 create or replace view v_ingredientRegime as (
     select r.id as `regimeId`,i.lib as ingredient, r.nomplat as regime, ir.pourcentage as pourcentage, i.prixG as prixG, r.`poidsTotalPlat` as poidsTotalPlat
     from ingredients i
